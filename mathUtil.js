ef8f912836fe2e75be9e0d92fba31667c9cd1483
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-04-18 18:26:14 +0800

    Add files via upload

---
+++ mathUtil.js
@@ -0,0 +1,14 @@
+
+export const PI = 3.14159;
+
+export function getCircumference(radius){
+    return 2 * PI * radius;
+}
+
+export function getArea(radius){
+    return PI * radius * radius;
+}
+
+export function getVolume(radius){
+    return 4 * PI * radius * radius;
+}
